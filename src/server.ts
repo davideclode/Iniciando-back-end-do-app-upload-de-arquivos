4c7b632a9d7e0ef9275c4317d12076cedd18004e
Davide da Silva <silvada1@hotmail.com>
2020-08-12 22:35:38 -0300

    chore: Serving static files

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express from 'express';
 // src/server.ts
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 // Importamos a conexão aqui. Como não precisamos nomear esta conexão, ou seja, como não vamos passar nenhum dado desse arquivo para outra variável então temos simplesmente que importar.
 import './database';
@@ -10,6 +11,9 @@ import './database';
 const app = express();
 
 app.use(express.json());
+
+// Toda rota que começa com o prefixo "file",  o que vem depois dela eu vou servir de forma estática. Ou seja, o que vem depois vai ser o nome do arquivo e vai mostrar para o usuário final exatamente o arquivo/ a imagem. Dentro do "static" vamos passar o endereço da pasta ou arquivo. Para isso, precisamos importar o "uploadConfig" do './config/upload';
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
